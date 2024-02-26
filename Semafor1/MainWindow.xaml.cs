18c1098ef7c761b52bf2e1298d0191564c3f6f91
rehmanli1470ww <144922823+rehmanli1470ww@users.noreply.github.com>
2024-02-26 22:23:54 +0400

    Add project files.

---
+++ Semafor1/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using EntityFremworkImtahan.Service;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Semafor1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, NotificationService
+    {
+        private ObservableCollection<Thread> watingthreads;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        public ObservableCollection<Thread> Workingthreads {  get; set; }
+        public ObservableCollection<Thread> Createthreads {  get; set; }
+        public ObservableCollection<Thread> Watingthreads { get => watingthreads; set { watingthreads = value; OnPropertyChanged(); } }
+        public Semaphore semaphore { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+           
+            Workingthreads = new ObservableCollection<Thread>();
+            Createthreads= new ObservableCollection<Thread>();
+            Watingthreads= new ObservableCollection<Thread>();
+            semaphore= new Semaphore(1,1,"Semafor");
+        }
+
+        public void Create(object sender, RoutedEventArgs e)
+        {
+            Thread thread = new Thread(() => {
+
+            });
+            thread.Name = "Thread" + thread.ManagedThreadId;
+            Createthreads.Add(thread);
+        }
+        public void Waiting_Thread(object sender, RoutedEventArgs e)
+        {
+            var IntialCount=int.Parse(numberTextBox.Text);
+            var ListItem = (ListView)sender;
+            var temp = (Thread)ListItem.SelectedItem;
+            semaphore = new Semaphore(IntialCount, IntialCount, "Semafor");
+            new Thread(() =>
+            {
+                Application.Current.Dispatcher.Invoke(new Action(() =>
+                {
+                    Watingthreads.Add(temp);
+                    Createthreads.Remove(temp);
+
+                }));
+
+
+                semaphore.WaitOne(2000);
+                Thread.Sleep(1000);
+                Application.Current.Dispatcher.Invoke(new Action(() =>
+                {
+                    Workingthreads.Add(temp);
+                    Watingthreads.Remove(temp);
+
+                }));
+
+
+                semaphore.Release();
+               
+            }).Start();
+            
+        }
+
+       
+
+    }
+}
---
+++ Semafor1/Service/NotificationService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFremworkImtahan.Service
+{
+    public interface NotificationService : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string? propertyName = null);
+    }
+}
